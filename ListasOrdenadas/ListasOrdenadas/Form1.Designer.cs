563db81d332ba1fe62b594c58f2c8e2383f6b291
Mildred Silva M <mildred_natali@hotmail.com>
2017-05-03 23:00:31 -0500

    Lista Ordenada

---
+++ ListasOrdenadas/ListasOrdenadas/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace ListasOrdenadas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AGREGAR = new System.Windows.Forms.Button();
+            this.ELIMINAR = new System.Windows.Forms.Button();
+            this.BUSCAR = new System.Windows.Forms.Button();
+            this.REPORTE = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // AGREGAR
+            // 
+            this.AGREGAR.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AGREGAR.Location = new System.Drawing.Point(36, 212);
+            this.AGREGAR.Name = "AGREGAR";
+            this.AGREGAR.Size = new System.Drawing.Size(85, 23);
+            this.AGREGAR.TabIndex = 0;
+            this.AGREGAR.Text = "+";
+            this.AGREGAR.UseVisualStyleBackColor = true;
+            this.AGREGAR.Click += new System.EventHandler(this.AGREGAR_Click);
+            // 
+            // ELIMINAR
+            // 
+            this.ELIMINAR.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ELIMINAR.Location = new System.Drawing.Point(152, 212);
+            this.ELIMINAR.Name = "ELIMINAR";
+            this.ELIMINAR.Size = new System.Drawing.Size(81, 24);
+            this.ELIMINAR.TabIndex = 1;
+            this.ELIMINAR.Text = "-";
+            this.ELIMINAR.UseVisualStyleBackColor = true;
+            this.ELIMINAR.Click += new System.EventHandler(this.ELIMINAR_Click);
+            // 
+            // BUSCAR
+            // 
+            this.BUSCAR.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BUSCAR.Location = new System.Drawing.Point(36, 293);
+            this.BUSCAR.Name = "BUSCAR";
+            this.BUSCAR.Size = new System.Drawing.Size(100, 24);
+            this.BUSCAR.TabIndex = 2;
+            this.BUSCAR.Text = "BUSCAR";
+            this.BUSCAR.UseVisualStyleBackColor = true;
+            this.BUSCAR.Click += new System.EventHandler(this.BUSCAR_Click);
+            // 
+            // REPORTE
+            // 
+            this.REPORTE.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.REPORTE.Location = new System.Drawing.Point(382, 387);
+            this.REPORTE.Name = "REPORTE";
+            this.REPORTE.Size = new System.Drawing.Size(100, 24);
+            this.REPORTE.TabIndex = 4;
+            this.REPORTE.Text = "REPORTE";
+            this.REPORTE.UseVisualStyleBackColor = true;
+            this.REPORTE.Click += new System.EventHandler(this.REPORTE_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(121, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(112, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(121, 95);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(112, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(121, 125);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(112, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(121, 155);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(112, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(259, 53);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(346, 315);
+            this.textBox5.TabIndex = 9;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "CODIGO: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(33, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 16);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "NOMBRE:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(33, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 16);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "COSTO:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(33, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 16);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "CANTIDAD:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(185, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(139, 18);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "LISTA ORDENADA ";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(152, 294);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(81, 22);
+            this.textBox6.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 507);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.REPORTE);
+            this.Controls.Add(this.BUSCAR);
+            this.Controls.Add(this.ELIMINAR);
+            this.Controls.Add(this.AGREGAR);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AGREGAR;
+        private System.Windows.Forms.Button ELIMINAR;
+        private System.Windows.Forms.Button BUSCAR;
+        private System.Windows.Forms.Button REPORTE;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ ListasOrdenadas/ListasOrdenadas/Lista.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasOrdenadas
+{
+    class Lista
+    {
+        public Nodo primero = null;
+        Nodo raiz;
+
+        public Lista()
+        {
+
+        }
+        //AGREGAR ORDENADO
+        public void agregar(Nodo nodo)
+        {
+            Nodo raiz = primero;
+            if (primero == null)
+            {
+                primero = nodo;
+
+
+            }
+            else
+            {
+                if (nodo.Codigo <= primero.Codigo)
+                {
+                    nodo.siguiente = primero;
+                    primero = nodo;
+                }
+
+
+                else
+                {
+                    while (raiz.siguiente != null)
+                    {
+                        if (nodo.Codigo <= raiz.siguiente.Codigo)
+                            break;
+                        raiz = raiz.siguiente;
+                    }
+                    nodo.siguiente = raiz.siguiente;
+                    raiz.siguiente = nodo;
+                }
+            }
+        }
+
+        
+        //BUSQUEDA
+        public Nodo buscar(int codigo)
+        {
+            Nodo search = primero;
+            while (search != null)
+            {
+                if (search.Codigo == codigo)
+                {
+                    return search;
+                }
+                else
+                {
+                    search = search.siguiente;
+                }
+            }
+            return null;
+        }
+        //ELIMINAR
+        public void eliminar(int codigo)
+        {
+            if (primero.Codigo == codigo)
+            {
+                primero = primero.siguiente;
+
+            }
+            else
+            {
+                Nodo temporal = primero;
+
+                while (temporal.siguiente != null)
+                {
+                    if (temporal.siguiente.Codigo == codigo)
+                    {
+                        temporal.siguiente = temporal.siguiente.siguiente;
+
+                        break;
+                    }
+                    else
+                    {
+                        temporal = temporal.siguiente;
+                    }
+                }
+            }
+
+
+        }
+        //LISTA
+        public string lista()
+        {
+            string show = ToString() + Environment.NewLine;
+            Nodo temporal = primero;
+            while (temporal != null)
+            {
+                show += " " + Environment.NewLine;
+                show += temporal.ToString();
+                temporal = temporal.siguiente;
+            }
+            return show;
+        }
+
+    }
+}
+
